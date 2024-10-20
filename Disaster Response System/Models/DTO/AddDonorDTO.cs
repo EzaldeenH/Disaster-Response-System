c2fb7a618ccb7558d72617eff3c7cd1d84e69619
EzaldeenH <129762820+EzaldeenH@users.noreply.github.com>
2024-10-20 05:53:24 +0300

    resest

--- Disaster Response System/Models/DTO/AddDonorDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿    namespace Disaster_Response_System.Models.DTO
-{
-    public class AddDonorDTO
-    {
-        public string Name { get; set; }
-        public string? Organization { get; set; }
-
-    }
-}
--- Disaster Response System/Models/DTO/RoundStatusDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Disaster_Response_System.Models.DTO
-{
-    public class RoundStatusDTO
-    {
-        public bool RoundActive { get; set; }
-    }
-}
-
